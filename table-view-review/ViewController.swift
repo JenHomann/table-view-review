094c59b4474c194304f108fd2700582824016ffb
Jen Homann <jenhomann@Jens-MBP.igs2.com>
2017-11-29 15:29:26 -0600

    basic tableview application listing 1-50

--- table-view-review/ViewController.swift
+++ table-view-review/ViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50;
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "numberCell")
+        
+        cell.textLabel?.text = String(indexPath.row + 1)
+        
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
